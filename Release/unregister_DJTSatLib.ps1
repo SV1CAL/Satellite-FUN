753a6a76ca2566c0a4a55509c71b5fb583684ab7
Michael <michael_margaras@yahoo.gr>
2024-01-23 17:56:20 +0200

    powershell to reg/unreg dll
    1v44

---
+++ Release/unregister_DJTSatLib.ps1
@@ -0,0 +1,6 @@
+$regsvrp = Start-Process regsvr32.exe -ArgumentList "/U /s DJTSatLib.dll" -PassThru
+$regsvrp.WaitForExit(5000) # Wait (up to) 5 seconds
+if($regsvrp.ExitCode -ne 0)
+{
+    Write-Warning "regsvr32 exited with error $($regsvrp.ExitCode)"
+}
